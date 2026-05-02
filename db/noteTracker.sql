c8740d0b00fc89b3259edf3bba7afbb8f303f231
WParkerHolland <parkerholland04@gmail.com>
2026-05-02 10:11:38 -0400

    Removed worldSecurity table

--- db/noteTracker.sql
+++ db/noteTracker.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Apr 20, 2026 at 10:26 PM
+-- Generation Time: May 02, 2026 at 08:53 AM
 -- Server version: 5.7.44-48
 -- PHP Version: 8.3.26
 
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `loganmcc_scribe`
+-- Database: `parkerho_noteTracker`
 --
 
 -- --------------------------------------------------------
@@ -44,18 +44,17 @@ CREATE TABLE `Characters` (
 --
 
 INSERT INTO `Characters` (`ID`, `isAt`, `name`, `race`, `description`, `gmNotes`, `partyNotes`, `img_src`, `visible`) VALUES
-(1, 2, 'Wilhelm Riquet', 'human', 'thief', 'Passion for animals', 'Played by Steve', NULL, 0),
+(1, 3, 'Wilhelm Riquet', 'human', 'thief', 'Passion for animals', 'Played by Steve', NULL, 0),
 (2, 4, 'Commodore Stephahk', 'human', 'Recruitment officer at Fort Hranic', 'Rat who let attackers into fort', 'Weirdly chill guy', NULL, 0),
 (3, 4, 'Renly Gokel', 'human', 'Recruit specializing in alchemy', 'Harmless?', 'He is an evil terrorist', NULL, 0),
-(4, 2, 'Horacio Garzon', 'human', 'wine artisan', 'Passion for grapes', 'Played by George', 'imgs/horacio.png', 0),
-(5, 2, 'Jovi', 'Ka’Tavin', 'Animal sold in Siwanilua', 'Will be used to train ‘Ride’ skill', 'Bought by Wilhelm', NULL, 0),
+(4, 3, 'Horacio Garzon', 'human', 'wine artisan', 'Passion for grapes', 'Played by George', 'imgs/horacio.png', 0),
+(5, 2, 'Jovi', 'Ka’Tavin', 'Animal sold in Siwanilua', 'Will be used to train ‘Ride’ skill', 'Bought by Wilhelm', NULL, 1),
 (6, 4, 'Olver Thumbless', 'human', 'Recruit who has no thumbs', 'Dies in Hranic Raid', 'His name is now ‘Nubs’', NULL, 0),
 (7, 5, 'Xiarkydoth', 'spider', 'Spider in Myrantahl Forests', 'Beast Aliyra encounters?', 'Hostile forest creature, dangerous but avoidable', NULL, 0),
 (8, 2, 'Leon Septar', 'human', 'Bouncer, detective', 'Passion for sneaking', 'Played by Henry', NULL, 0),
 (9, 5, 'Aliyra Maastehr', 'ghord', 'Apothecary from Ghordeiol', 'Wife to Ephram and Mother to Obram and Ilen', 'Helpful apothecary, potential ally for potions and healing', NULL, 0),
 (10, 4, 'Chef Mya', 'human', 'Chef at Fort Hranic', 'Head chef', 'Falls in love with Wilhelm', NULL, 0),
-(11, 2, '', '', '', '', 'Cool cat that we must obtain', NULL, 0),
-(12, 2, 'Healer Mam', 'elf', 'Decorated in trinkets and baubles', 'All poisons', 'Healer', NULL, 0),
+(12, 2, 'Healer Mam', 'elf', 'Decorated in trinkets and baubles', 'All poisons', 'Healer', NULL, 1),
 (13, 2, 'Bram Ironfist', 'dwarf', 'Blacksmith in Fort Hranic', 'Provides weapons to guards', 'Gruff but reliable', NULL, 0),
 (14, 3, 'Lysa Windmere', 'human', 'Traveling merchant', 'Knows trade routes', 'Often visits taverns', NULL, 0),
 (15, 5, 'Torren Vale', 'elf', 'Forest scout', 'Watches for threats in Myrantahl Forest', 'Quiet and observant', NULL, 0),
@@ -65,7 +64,9 @@ INSERT INTO `Characters` (`ID`, `isAt`, `name`, `race`, `description`, `gmNotes`
 (19, 10, 'Sanjal', 'Kua Lorantene', 'Leader of the Syndicate of Siwanilua', 'Working to take down slaveowners', '', 'imgs/npc_default.png', 1),
 (20, 3, 'Priest Alquiv', '', 'The tender of the gardens and worshipper of Azkenilua', '', '', 'imgs/npc_default.png', 1),
 (21, 11, 'Nili', '', 'Member of Syndicate of Siwanilua', '', 'Hold', 'imgs/npc_default.png', 0),
-(22, 12, 'Nij\'Gama', '', 'Massive tortoise that buries in the sand dunes', '', '', 'imgs/creature_default.png', 0);
+(22, 12, 'Nij\'Gama', '', 'Massive tortoise that buries in the sand dunes', '', '', 'imgs/creature_default.png', 0),
+(23, 1, 'A Starman', '', 'Waiting in the sky', '', '', 'imgs/npc_default.png', 0),
+(24, 13, 'Innkeeper Sivil', '', 'The innkeep and bartender', '', '', 'imgs/npc_default.png', 1);
 
 -- --------------------------------------------------------
 
@@ -89,9 +90,11 @@ INSERT INTO `Contains` (`container`, `containee`) VALUES
 (1, 12),
 (2, 3),
 (2, 10),
+(2, 13),
 (4, 7),
 (6, 1),
-(6, 5);
+(6, 5),
+(13, 14);
 
 -- --------------------------------------------------------
 
@@ -112,26 +115,10 @@ CREATE TABLE `Creatures` (
 INSERT INTO `Creatures` (`ID`, `population`, `ability`) VALUES
 (5, 1, 'Pincer Tail Attack'),
 (7, 1, 'Sticky Web Grab'),
-(11, 1000, 'Jumping Claw Grab'),
 (15, 30, 'Bite Attack'),
 (16, 10, 'Venom Strike'),
 (17, 60, 'Camouflage'),
 (22, 0, '');
-
--- --------------------------------------------------------
-
---
--- Stand-in structure for view `dm_inventory_view`
--- (See below for the actual view)
---
-CREATE TABLE `dm_inventory_view` (
-`OwnerName` varchar(50)
-,`ItemName` varchar(50)
-,`ItemType` varchar(50)
-,`ItemDescription` varchar(1000)
-,`ItemNotes` varchar(1000)
-,`QuantityOwned` int(11)
-);
 
 -- --------------------------------------------------------
 
@@ -165,7 +152,9 @@ INSERT INTO `Locations` (`ID`, `name`, `description`, `gmNotes`, `partyNotes`, `
 (9, 'Roshar', 'World where Stormlight Archives takes place', 'World for Stormlight tabletop RPG', 'Populations range from different kinds of Humans to crab people called Parshendi', NULL, 0),
 (10, 'The Back of the Stage Tavern', 'Poor tavern in the Slums District of Siwanilua', '', '', 'imgs/location_default.png', 1),
 (11, 'Ahn Janil', 'Small village northwest of Siwanilua', '', '', 'imgs/location_default.png', 0),
-(12, 'Kalikiv Kalin Dunes', 'Sand dunes home of the Nij\'Gama', '', '', 'imgs/location_default.png', 1);
+(12, 'Kalikiv Kalin Dunes', 'Sand dunes home of the Nij\'Gama', '', '', 'imgs/location_default.png', 1),
+(13, 'The Scarab and the Snake Tavern', 'A tavern in the upper Market District', '', '', 'imgs/location_default.png', 1),
+(14, 'The Secret Tunnels', 'They have a puzzle, a combat encounter, and its all a secret', '', '', 'imgs/location_default.png', 0);
 
 -- --------------------------------------------------------
 
@@ -190,11 +179,12 @@ INSERT INTO `NPCs` (`ID`, `opinions`, `occupation`, `gold`) VALUES
 (6, 'Eager to prove himself despite disability', 'Chef in Training', 0),
 (9, 'Caring and knowledgeable, misses her family', 'Apothecary', 0),
 (10, 'Warm and welcoming, develops feelings for Wilhelm', 'Chef', 50),
-(11, 'Strict but fair leader of the guards', 'Guard Captain', 0),
 (12, 'Kind healer who helps the poor', 'Healer', 0),
 (19, '', '', 50),
 (20, '', '', 0),
-(21, '', '', 0);
+(21, '', '', 0),
+(23, '', 'a Starman', 0),
+(24, '', '', 0);
 
 -- --------------------------------------------------------
 
@@ -217,20 +207,6 @@ INSERT INTO `Players` (`ID`, `level`, `playedBy`, `combat_style`) VALUES
 (1, 3, 'Steve', 'Hracian Street Rat'),
 (4, 3, 'George', 'Hracian Recruit'),
 (8, 3, 'Henry', 'Hracian Bad Cop');
-
--- --------------------------------------------------------
-
---
--- Stand-in structure for view `player_NPC_view`
--- (See below for the actual view)
---
-CREATE TABLE `player_NPC_view` (
-`Name` varchar(50)
-,`Race` varchar(50)
-,`Description` varchar(1000)
-,`Notes` varchar(1000)
-,`Location` varchar(50)
-);
 
 -- --------------------------------------------------------
 
@@ -282,9 +258,9 @@ CREATE TABLE `Props` (
 --
 
 INSERT INTO `Props` (`ID`, `isIn`, `name`, `description`, `gmNotes`, `partyNotes`, `itemType`, `rarity`, `quantity`, `owner`, `img_src`, `visible`) VALUES
-(1, 2, 'Venom Spike', 'A spike harvested from a Kua Lorantene warrior', 'Can be used as a poison weapon', 'Found in Siwanilua market', 'Weapon', 'Uncommon', 3, 1, NULL, 0),
+(1, 2, 'Venom Spike', 'A spike harvested from a Kua Lorantene warrior', 'Can be used as a poison weapon', 'Found in Siwanilua market', 'Weapon', 'Uncommon', 3, 1, NULL, 1),
 (2, 4, 'Alchemy Kit', 'A set of tools for brewing potions', 'Belongs to Renly', 'Confiscated after the raid', 'Tool', 'Common', 1, 3, NULL, 0),
-(3, 2, 'Grape Wine', 'A fine bottle of Siwaniluan wine', 'Horacio made this himself', 'Worth good money', 'Consumable', 'Common', 5, 4, NULL, 0),
+(3, 2, 'Grape Wine', 'A fine bottle of Siwaniluan wine', 'Horacio made this himself', 'Worth good money', 'Consumable', 'Common', 5, 4, NULL, 1),
 (4, 4, 'Fort Manifest', 'A list of all recruits at Hranic Fort', 'Contains evidence against Stephahk', 'We need to get this', 'Document', 'Rare', 1, 2, NULL, 0),
 (5, 1, 'Desert Cloak', 'A cloak that blends into sand', 'Useful for desert travel', 'Bought in Kua Loranta', 'Armor', 'Common', 1, 8, NULL, 1),
 (6, 4, 'Iron Sword', 'Standard weapon used by guards', 'Standard issue weapon kept in the fort armory', 'Useful if we need extra weapons', 'Weapon', 'Common', 1, 1, NULL, 0),
@@ -300,7 +276,11 @@ INSERT INTO `Props` (`ID`, `isIn`, `name`, `description`, `gmNotes`, `partyNotes
 (16, 1, 'Rope', '1 foot of hempen rope', '', '', 'Tool', 'Common', 50, 8, NULL, 1),
 (17, 1, 'Ancient Sword', 'There is myth of an ancient sword somewhere lost in this nation', 'Buried under a tree', '', 'Weapon', 'Rare', 1, NULL, NULL, 0),
 (18, 1, 'Glasswork', 'Glassblowing is a prized art here, so it can be quite valuable when done well.', '', '', 'Miscellaneous', 'Uncommon', 4, NULL, NULL, 0),
-(19, 10, 'Mr. Bones', 'A skeleton sitting on a chair', '', '', '', '', 1, NULL, 'imgs/props_default.png', 1);
+(19, 10, 'Mr. Bones', 'A skeleton sitting on a chair', '', '', '', '', 1, NULL, 'imgs/props_default.png', 1),
+(20, 13, 'Tivian\'s Famous Wine', 'A competing brand to Horacio Garzon', '', '', '', '', 1, NULL, 'imgs/props_default.png', 1),
+(21, 3, 'Grapes Plants', 'it is what it is', '', '', '', '', 1, NULL, 'imgs/props_default.png', 1),
+(22, 3, 'Statue to Azkenilua', 'The Mother of Time', '', '', '', '', 1, NULL, 'imgs/props_default.png', 1),
+(23, 3, 'Bythemen', 'Its a plant. And its poisonous.', '', '', '', '', 1, NULL, 'imgs/props_default.png', 0);
 
 -- --------------------------------------------------------
 
@@ -316,32 +296,13 @@ CREATE TABLE `Users` (
 --
 -- Dumping data for table `Users`
 --
+
 INSERT INTO `Users` (`username`, `password`) VALUES
 ('George', '$2y$12$.LGrUxPef5ynxKll25SAGOdWO5gtvL109p34ShGItQWaVcaOTpBfq'),
 ('GM Individual', '$2y$12$SpzkhrJh8sdTfnX7dOtlNeMjM7FZIcueFsCFUoqDeiX33Mp0JkjYu'),
 ('Henry', '$2y$12$2eFy.SQ9qEwoBRbmYZUfauTRN39raP1vTfjmDXmdC6m0Q0IHReDIS'),
 ('No Friends Larry', '$2y$12$C27ZHCkDCDswp9bVVAqXZeBLfIex3rqrziz8973RF6srjGopsT/.e'),
 ('Steve', '$2y$12$SpzkhrJh8sdTfnX7dOtlNeMjM7FZIcueFsCFUoqDeiX33Mp0JkjYu');
-
-
-
--- --------------------------------------------------------
-
---
--- Table structure for table `WorldSecurity`
---
-
-CREATE TABLE `WorldSecurity` (
-  `worldID` int(11) NOT NULL,
-  `securityMeasure` enum('plain','hash') COLLATE utf8_unicode_ci NOT NULL DEFAULT 'plain'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
---
--- Dumping data for table `WorldSecurity`
---
-
-INSERT INTO `WorldSecurity` (`worldID`, `securityMeasure`) VALUES
-(6, 'plain');
 
 --
 -- Indexes for dumped tables
@@ -409,12 +370,6 @@ ALTER TABLE `Users`
   ADD PRIMARY KEY (`username`);
 
 --
--- Indexes for table `WorldSecurity`
---
-ALTER TABLE `WorldSecurity`
-  ADD PRIMARY KEY (`worldID`);
-
---
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -422,37 +377,19 @@ ALTER TABLE `WorldSecurity`
 -- AUTO_INCREMENT for table `Characters`
 --
 ALTER TABLE `Characters`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 
 --
 -- AUTO_INCREMENT for table `Locations`
 --
 ALTER TABLE `Locations`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT for table `Props`
 --
 ALTER TABLE `Props`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
-
--- --------------------------------------------------------
-
---
--- Structure for view `dm_inventory_view`
---
-DROP TABLE IF EXISTS `dm_inventory_view`;
-
-CREATE ALGORITHM=UNDEFINED DEFINER=`loganmccue`@`localhost` SQL SECURITY DEFINER VIEW `dm_inventory_view`  AS SELECT `Characters`.`name` AS `OwnerName`, `Props`.`name` AS `ItemName`, `Props`.`itemType` AS `ItemType`, `Props`.`description` AS `ItemDescription`, `Props`.`gmNotes` AS `ItemNotes`, `Props`.`quantity` AS `QuantityOwned` FROM (`Characters` join `Props`) WHERE (`Props`.`owner` = `Characters`.`ID`) ORDER BY `Characters`.`name` ASC ;
-
--- --------------------------------------------------------
-
---
--- Structure for view `player_NPC_view`
---
-DROP TABLE IF EXISTS `player_NPC_view`;
-
-CREATE ALGORITHM=UNDEFINED DEFINER=`loganmccue`@`localhost` SQL SECURITY DEFINER VIEW `player_NPC_view`  AS SELECT `Characters`.`name` AS `Name`, `Characters`.`race` AS `Race`, `Characters`.`description` AS `Description`, `Characters`.`partyNotes` AS `Notes`, `Locations`.`name` AS `Location` FROM (`Characters` join `Locations`) WHERE ((`Characters`.`isAt` = `Locations`.`ID`) AND (not(`Characters`.`ID` in (select `Players`.`ID` from `Players`)))) ORDER BY `Locations`.`name` ASC ;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 
 --
 -- Constraints for dumped tables
@@ -494,8 +431,9 @@ ALTER TABLE `Players`
 -- Constraints for table `PlayingIn`
 --
 ALTER TABLE `PlayingIn`
-  ADD CONSTRAINT `inWorld` FOREIGN KEY (`world`) REFERENCES `Locations` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `user` FOREIGN KEY (`user`) REFERENCES `Users` (`username`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+  ADD CONSTRAINT `isUser` FOREIGN KEY (`user`) REFERENCES `Users` (`username`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `plays` FOREIGN KEY (`plays`) REFERENCES `Characters` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `playsIn` FOREIGN KEY (`world`) REFERENCES `Locations` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
 -- Constraints for table `Props`
@@ -503,12 +441,6 @@ ALTER TABLE `PlayingIn`
 ALTER TABLE `Props`
   ADD CONSTRAINT `isIn` FOREIGN KEY (`isIn`) REFERENCES `Locations` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   ADD CONSTRAINT `ownedBy` FOREIGN KEY (`owner`) REFERENCES `Characters` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION;
-
---
--- Constraints for table `WorldSecurity`
---
-ALTER TABLE `WorldSecurity`
-  ADD CONSTRAINT `isWorld` FOREIGN KEY (`worldID`) REFERENCES `PlayingIn` (`world`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
